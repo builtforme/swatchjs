68db04ab2bb31cadb9c41e78a743f7eae999f228
Brian Timm <bwtimm@users.noreply.github.com>
2017-08-17 09:14:47 -0700

    Create split methods for parsing params vs executing handers (#24)
    * Create separate swatch methods for parse and handle
    * Major version bump for breaking changes
    * Validate method should return list of params and dict from name to value

--- test/loader.test.js
+++ test/loader.test.js
@@ -3,9 +3,10 @@ const load = require('../lib/loader');
 
 function validate(model) {
   model.forEach((method) => {
-    expect(method).to.be.an('object').that.has.all.keys('name', 'handle', 'metadata');
+    expect(method).to.be.an('object').that.has.all.keys('name', 'handle', 'validate', 'metadata');
     expect(method.name).to.be.a('string');
     expect(method.handle).to.be.a('function');
+    expect(method.validate).to.be.a('function');
 
     expect(method.metadata).to.be.an('object').that.has.all.keys('noAuth', 'middleware');
     expect(method.metadata.noAuth).to.be.an('boolean');
